7fdbe5ee98d038ec2942bc65d107ffb056ba8a95
Pseudo <134837403+Pse-do@users.noreply.github.com>
2023-09-01 22:03:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let sumbitBtn = document.getElementById("sumbitBtn");
+let userGuess = document.getElementById("userGuess");
+const randomNo = Math.floor((Math.random() * 10) + 1);
+var noGuesses = 1;
+
+sumbitBtn.onclick = function() {
+    switch(true){
+        case Number(userGuess.value) == randomNo:
+            alert(`Correct the number was ${randomNo}, and you got it in ${noGuesses} guesses!`)
+            location.reload(); // reloads the page!
+            break;
+        case userGuess.value == "":
+            alert("Enter a fucking number lmao")
+            break;
+        case Number(userGuess.value) > 10 || Number(userGuess.value) < 1:
+            alert("The number must be between 1 and 10");
+            break;
+        default:
+            alert("Wrong try guess again!");
+            noGuesses += 1 
+    }
+    
+}
+
