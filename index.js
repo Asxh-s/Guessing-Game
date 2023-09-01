e80a5ce09721aaac2c116e013eb8cda31d8f1ea2
Pseudo <134837403+Pse-do@users.noreply.github.com>
2023-09-01 23:18:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-let sumbitBtn = document.getElementById("sumbitBtn");
-let userGuess = document.getElementById("userGuess");
+let submitBtn = document.getElementById("submitBtn");
+let userGuess = document.getElementById("userInputGuess");
 const randomNo = Math.floor((Math.random() * 10) + 1);
 var noGuesses = 1;
 
-sumbitBtn.onclick = function() {
-    console.log(userGuess.value)
+submitBtn.onclick = function() {
     switch(true){
         case Number(userGuess.value) == randomNo:
-            alert(`Correct the number was ${randomNo}, and you got it in ${noGuesses} guesses!`)
+            alert(`Correct the number was ${randomNo}, and you got it in ${noGuesses} guesses!`);
             location.reload(); // reloads the page!
             break;
         case userGuess.value == "" || userGuess.value === undefined:
-            alert("Enter a fucking number lmao")
+            alert("Please enter a number!")
             break;
         case Number(userGuess.value) > 10 || Number(userGuess.value) < 1:
             alert("The number must be between 1 and 10");
